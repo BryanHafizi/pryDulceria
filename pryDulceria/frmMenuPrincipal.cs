d64e335cb30ba63e68e3b3adeb034d87b71426a9
diego <20251165@uthh.edu.mx>
2026-07-13 23:43:28 -0600

    menu principal logica

--- pryDulceria/frmMenuPrincipal.cs
+++ pryDulceria/frmMenuPrincipal.cs
@@ -1,13 +1,53 @@
 ﻿using System;
+using System.Security.Principal;
 using System.Windows.Forms;
 
 namespace pryDulceria
 {
     public partial class frmMenuPrincipal : Form
     {
+        clsMenuPrincipal principal;
         public frmMenuPrincipal()
         {
             InitializeComponent();
         }
+        private void pcbGeneral_MouseDown(object pictureSeleccionado, MouseEventArgs e)
+        {
+            PictureBox pcb = (PictureBox)pictureSeleccionado;
+            pcb.Location = new Point(pcb.Location.X + 3, pcb.Location.Y + 3);
+        }
+        private void pcbGeneral_MouseUp(object pictureSeleccionado, MouseEventArgs e)
+        {
+            PictureBox pcb = (PictureBox)pictureSeleccionado;
+            pcb.Location = new Point(pcb.Location.X - 3, pcb.Location.Y - 3);
+
+        }
+
+        private void pbVentas_Click(object sender, EventArgs e)
+        {
+            principal = new clsMenuPrincipal();
+            principal.agregarAlContenedor(new frmVentas(), pnlContenedor);
+        }
+        private void pbInventarios_Click(object sender, EventArgs e)
+        {
+            principal = new clsMenuPrincipal();
+            principal.agregarAlContenedor(new frmInventario(), pnlContenedor);
+        }
+        /*private void pbReportes_Click(object sender, EventArgs e)
+        {
+            principal = new clsMenuPrincipal();
+            principal.agregarAlContenedor(new frm(), pnlContenedor);
+        }*/
+        private void pbProveedores_Click(object sender, EventArgs e)
+        {
+            principal = new clsMenuPrincipal();
+            principal.agregarAlContenedor(new frmProveedores(), pnlContenedor);
+        }
+
+        private void pbReportes_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
