c642609437b22896ae4c21b5bea6993ff4f94f50
Jhoswan Clemente Hernández Hernández <jhoswanhernandez@gmail.com>
2026-07-29 20:39:24 -0600

    xd

--- pryDulceria/frmCategorias.cs
+++ pryDulceria/frmCategorias.cs
@@ -67,69 +67,18 @@ namespace pryDulceria
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
-            if (idCategoriaSeleccionada == 0)
-            {
-                MessageBox.Show("Por favor, selecciona una categoría de la tabla para eliminar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-            DialogResult respuesta = MessageBox.Show("¿Estás seguro de que deseas eliminar esta categoría?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-
-            if (respuesta == DialogResult.Yes)
-            {
-                try
-                {
-                    categoria.Id_categoria = idCategoriaSeleccionada;
-                    MessageBox.Show(categoria.Eliminar(), "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    txtNombre.Clear();
-                    idCategoriaSeleccionada = 0;
-                    CargarGrid();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
+          
 
         }
 
         private void frmCategorias_Load(object sender, EventArgs e)
         {
-            CargarGrid();
+         
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(txtNombre.Text))
-            {
-                MessageBox.Show("¡Ingrese un nombre valido!.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-            try
-            {
-                string msg = "";
-
-                if (idCategoriaSeleccionada == 0)
-                {
-                    categoria.Categoria = txtNombre.Text;
-                    msg = categoria.AgregarCategoria();
-                }
-                else
-                {
-                    categoria.Id_categoria = idCategoriaSeleccionada;
-                    categoria.Categoria = txtNombre.Text;
-                    msg = categoria.Actualizar();
-                }
-                MessageBox.Show(msg, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                txtNombre.Clear();
-                idCategoriaSeleccionada = 0;
-                CargarGrid();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ocurrió un error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
+            
         }
     }
 }
